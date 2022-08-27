db7850fa02486279c7c805537216361796fad12e
andrematheus2 <am_arao@hotmail.com>
2022-08-27 10:36:28 -0300

    inserindo no banco com parametros usando o PreparedStatement

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -1,18 +1,23 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestaInsercao {
+public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-
+		String nome = "Mouse'";
+		String descricao = "Razer); delete from produto;";
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recuperaConexao();
 
-		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Razer')", 
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", 
 				Statement.RETURN_GENERATED_KEYS);
+		
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		stm.execute();
 		
 		ResultSet rst = stm.getGeneratedKeys();
 		while(rst.next()) {
